6eaa2d7aa35e26b9c22e5648f50466a9d3516c43
vishalpokuri <vishal.pokuri@gmail.com>
2025-05-15 23:54:53 +0530

    Posthog integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+posthog.init("phc_cHH8RPwyQVB4wMHRzbQAe5s5q0q4g1mOaML8dba37ZC", {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+});
+
+createRoot(document.getElementById("root")!).render(
+  <PostHogProvider client={posthog}>
+    <App />
+  </PostHogProvider>
+);
